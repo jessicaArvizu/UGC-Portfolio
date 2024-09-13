b54fb2e6a3802bd5eacb8db461cd0052000c6724
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-09-12 22:16:02 -0600

    Update Header.tsx
    For mobile

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,14 +5,14 @@ import Star2 from "@/components/ui/star2-icon";
 
 export default function Header() {
   return (
-    <header className="inset-0 py-10 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-[#FF8ACD] bg-[linear-gradient(to_right,#FFC6E733_2px,transparent_1px),linear-gradient(to_bottom,#FFC6E733_2px,transparent_1px)] bg-[size:70px_70px]">
-      <div className="mx-auto max-w-full px-8 py-10 text-center lg:py-[30px] bg-white rounded-xl border-4 border-black flex flex-col lg:flex-row h-full shadow-[8px_8px_0px_#000] relative">
+    <header className="inset-0 p-10 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-[#FF8ACD] bg-[linear-gradient(to_right,#FFC6E733_2px,transparent_1px),linear-gradient(to_bottom,#FFC6E733_2px,transparent_1px)] bg-[size:70px_70px]">
+      <div className="mx-automax-w-full px-8 py-10 text-center lg:py-[30px] bg-white rounded-xl border-4 border-black flex flex-col lg:flex-row h-full shadow-[8px_8px_0px_#000] relative">
 
         <div className="absolute -top-10 -left-10 lg:-top-20 lg:-left-20">
           <Star2 color='#FFEA80' size='150px' />
         </div>
 
-        <div className="flex flex-col justify-between w-full lg:w-2/3 h-full space-y-3">
+        <div className="flex flex-col justify-between w-full mt-20 lg:mt-1 lg:w-2/3 h-full space-y-3">
           <div className="flex flex-col items-center justify-center">
             <h2 className="text-2xl font-heading md:text-3xl lg:text-4xl">
               Hola, soy
