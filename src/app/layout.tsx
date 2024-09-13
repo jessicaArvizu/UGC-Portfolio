2639b665ad83abfe65f810a57a4d050f9c4c7208
Jessica Arvizu <jessica.arvizuto@gmail.com>
2024-09-12 21:38:24 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Lexend_Mega } from "next/font/google";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Jessica Arvizu UGC",
